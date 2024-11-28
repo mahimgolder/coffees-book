7b0bf9b334ba1edf02d8e401e3f9112c82ec7bc2
mahimgolder <mahimgolder@mahim>
2024-11-28 20:10:21 +0600

    setup react route, tailwindCss, daisyui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  return (
+    <>
+      
+      <p className="text-6xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
